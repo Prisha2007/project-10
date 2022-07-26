ea1d256b7664c11e2ebd7303abe490bf82743153
Prisha2007 <109626196+Prisha2007@users.noreply.github.com>
2022-07-26 15:44:42 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var plum = [35,45,55,65]
+var sum = plum[0]+plum [1]+plum[2]+plum [3]
+var avg = sum/plum.length
+console.log(avg)
+function setup() 
+{
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+background(51);
+
+}
+
